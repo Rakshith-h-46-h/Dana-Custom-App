87a7821b7e5a413571a570bbca8d85b71aa92d91
Rakshith <rakshith.h46@gmail.com>
2026-07-10 14:00:21 +0530

    Initial commit

---
+++ dana/handler/components/mainForm.js
@@ -0,0 +1,23 @@
+import { generateField, addDynamicBehavior } from '../templates/form.template.js';
+// import PayloadData from '../app.js';
+
+export function renderForm(formData, payloadData) {
+    const formContainer = document.getElementById('formContainer');
+    let formHtml = '<form id="customForm">';
+
+    window.PayloadData = payloadData;
+
+    console.log("window.PayloadData object", window.PayloadData)
+    console.log("formData", formData)
+
+    // Generate and show the first layer dropdown
+    formHtml += generateField(formData, true, false);
+
+
+    formHtml += '<button type="submit">Submit</button>';
+    formHtml += '</form>';
+
+    formContainer.innerHTML = formHtml;
+
+    addDynamicBehavior(formData);
+}
