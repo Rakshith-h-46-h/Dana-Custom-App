101fb4416d98a9c8266563b82f4e6cdbde65209c
Rakshith-h-46-h <rakshith.h46@gmail.com>
2026-07-18 09:54:35 +0530

    Update mainForm.js

--- handler/components/mainForm.js
+++ handler/components/mainForm.js
@@ -1,23 +1,51 @@
 import { generateField, addDynamicBehavior } from '../templates/form.template.js';
 // import PayloadData from '../app.js';
 
+// export function renderForm(formData, payloadData) {
+//     const formContainer = document.getElementById('formContainer');
+//     let formHtml = '<form id="customForm">';
+
+//     window.PayloadData = payloadData;
+
+//     console.log("window.PayloadData object", window.PayloadData)
+//     console.log("formData", formData)
+
+//     // Generate and show the first layer dropdown
+//     formHtml += generateField(formData, true, false);
+
+
+//     formHtml += '<button type="submit">Submit</button>';
+//     formHtml += '</form>';
+
+//     formContainer.innerHTML = formHtml;
+
+//     addDynamicBehavior(formData);
+// }
 export function renderForm(formData, payloadData) {
     const formContainer = document.getElementById('formContainer');
     let formHtml = '<form id="customForm">';
 
     window.PayloadData = payloadData;
 
-    console.log("window.PayloadData object", window.PayloadData)
-    console.log("formData", formData)
+    console.log("formData:", formData);
 
-    // Generate and show the first layer dropdown
-    formHtml += generateField(formData, true, false);
+    const generatedHtml = generateField(formData, true, false);
 
+    console.log("Generated HTML:");
+    console.log(generatedHtml);
+
+    formHtml += generatedHtml;
 
     formHtml += '<button type="submit">Submit</button>';
     formHtml += '</form>';
 
+    console.log("Final HTML:");
+    console.log(formHtml);
+
     formContainer.innerHTML = formHtml;
+
+    console.log("Rendered HTML:");
+    console.log(formContainer.innerHTML);
 
     addDynamicBehavior(formData);
 }
