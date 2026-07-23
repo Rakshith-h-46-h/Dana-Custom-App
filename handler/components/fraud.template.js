9f6114f9b6ce075fee40d1aecdc10e1e5b7c2681
Rakshith-h-46-h <rakshith.h46@gmail.com>
2026-07-23 12:31:36 +0530

    Remove addDefaultFieldsEventListener function
    Removed unused event listener for default fields.

--- handler/components/fraud.template.js
+++ handler/components/fraud.template.js
@@ -1,7 +1,4 @@
 import { generateCustomField } from "./util.js";
-import { loadTeamTemplate } from "../app.js";
-
-const TEAM_CF_KEY = "k19";
 
 export function generateDefaultFields() {
     let fieldHtml = "";
@@ -15,32 +12,4 @@ export function generateDefaultFields() {
     });
 
     return fieldHtml;
-}
-
-export function addDefaultFieldsEventListener() {
-    const formContainer = document.getElementById("formContainer");
-
-    formContainer.addEventListener("change", async (event) => {
-        const eventTarget = event.target;
-
-        if (eventTarget.id !== TEAM_CF_KEY) return;
-
-        switch (eventTarget.value) {
-            case "Channel":
-                await loadTeamTemplate("channel");
-                break;
-
-            case "Fraud":
-                await loadTeamTemplate("fraud");
-                break;
-
-            case "Merchant":
-                await loadTeamTemplate("merchant");
-                break;
-
-            case "Non Fraud":
-                await loadTeamTemplate("non-fraud");
-                break;
-        }
-    });
 }
