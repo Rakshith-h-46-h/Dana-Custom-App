70e1f24570ab928fa5d746e90618df8d8e05dac4
Rakshith-h-46-h <rakshith.h46@gmail.com>
2026-07-23 11:14:06 +0530

    Refactor fraud.template.js for improved readability
    Refactor code for consistency and clarity in event listener and field generation.

--- handler/components/fraud.template.js
+++ handler/components/fraud.template.js
@@ -1,46 +1,46 @@
-import {generateCustomField} from "./util.js";
+import { generateCustomField } from "./util.js";
+import { loadTeamTemplate } from "../app.js";
+
 const TEAM_CF_KEY = "k19";
 
-
 export function generateDefaultFields() {
-  let fieldHtml = '';
+    let fieldHtml = "";
 
-  window.DefaultFieldsData.forEach(defaultField => {
-    fieldHtml += generateCustomField(defaultField.key, "default-field-container", defaultField.isRequired);
-  })
+    window.DefaultFieldsData.forEach(defaultField => {
+        fieldHtml += generateCustomField(
+            defaultField.key,
+            "default-field-container",
+            defaultField.isRequired
+        );
+    });
 
-  return fieldHtml;
-}
-
-function generateChannelCustomFields() {
-  let fieldHtml = '';
-  return fieldHtml;
+    return fieldHtml;
 }
 
 export function addDefaultFieldsEventListener() {
-  const formContainer = document.getElementById('formContainer');
+    const formContainer = document.getElementById("formContainer");
 
-formContainer.addEventListener("change", async (event) => {
-    const eventTarget = event.target;
+    formContainer.addEventListener("change", async (event) => {
+        const eventTarget = event.target;
 
-    if (eventTarget.id !== "k19") return;
+        if (eventTarget.id !== TEAM_CF_KEY) return;
 
-    switch (eventTarget.value) {
-        case "Channel":
-            await loadTeamTemplate("channel.json");
-            break;
+        switch (eventTarget.value) {
+            case "Channel":
+                await loadTeamTemplate("channel");
+                break;
 
-        case "Fraud":
-            await loadTeamTemplate("fraud.json");
-            break;
+            case "Fraud":
+                await loadTeamTemplate("fraud");
+                break;
 
-        case "Merchant":
-            await loadTeamTemplate("merchant.json");
-            break;
+            case "Merchant":
+                await loadTeamTemplate("merchant");
+                break;
 
-        case "Non Fraud":
-            await loadTeamTemplate("non-fraud.json");
-            break;
-    }
-});
+            case "Non Fraud":
+                await loadTeamTemplate("non-fraud");
+                break;
+        }
+    });
 }
