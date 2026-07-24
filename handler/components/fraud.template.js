98ab30d10b9504ff21a38c1cf8c26f224946e8cb
Rakshith-h-46-h <rakshith.h46@gmail.com>
2026-07-24 10:19:56 +0530

    Add generateCaseFields function to handle case fields

--- handler/components/fraud.template.js
+++ handler/components/fraud.template.js
@@ -18,3 +18,20 @@ window.DefaultFieldsData.defaultFields.forEach(defaultField => {
 
     return fieldHtml;
 }
+export function generateCaseFields(selectedCase) {
+    let fieldHtml = "";
+
+    const caseFields = window.DefaultFieldsData.cases[selectedCase];
+
+    if (!caseFields) return "";
+
+    caseFields.forEach(field => {
+        fieldHtml += generateCustomField(
+            field.key,
+            "case-field-container",
+            field.isRequired
+        );
+    });
+
+    return fieldHtml;
+}
