ee48fe28c6ec659b47fc0382027254e604972553
Rakshith-h-46-h <rakshith.h46@gmail.com>
2026-07-28 21:12:27 +0530

    Fix import statement formatting in main_form.js

--- handler/components/main_form.js
+++ handler/components/main_form.js
@@ -1,6 +1,7 @@
 import {
     generateDefaultFields,
-    addDefaultFieldsEventListener
+    addDefaultFieldsEventListener,
+    loadCaseFields
 } from "./fraud.template.js";
 import {addSubmitFormEventListener} from "../services/service_v2.js";
 
